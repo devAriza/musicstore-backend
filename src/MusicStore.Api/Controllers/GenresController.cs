98060d15402cd838aa0d85617f6c04f0a860f98d
JAriza <v.ariza.0511@gmail.com>
2025-06-29 18:20:56 -0600

    MS-01 More operations in memory (CRUD)

--- src/MusicStore.Api/Controllers/GenresController.cs
+++ src/MusicStore.Api/Controllers/GenresController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MusicStore.Entities;
 using MusicStore.Repositories;
 
 namespace MusicStore.Api.Controllers;
@@ -15,6 +16,41 @@ public class GenresController : ControllerBase
 
         this.repository = repository;
         
+    }
+
+    [HttpGet]
+    public ActionResult<List<Genre>> Get()
+    {
+        var data = repository.Get();
+        return Ok(data);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Genre> Get(int id)
+    {        
+        var item = repository.Get(id);
+        return item is not null ? Ok(item) : NotFound();
+    }
+
+    [HttpPost]
+    public ActionResult<Genre> Post(Genre genre)
+    {
+        repository.Add(genre);
+        return Ok(genre);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Genre genre)
+    {
+        repository.Update(id, genre);
+        return Ok();
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        repository.Delete(id);
+        return Ok();
     }
 
 }
