3cff6837b68d2885c6b33c8db0a934ac1074cefe
JAriza <v.ariza.0511@gmail.com>
2025-07-16 22:06:04 -0600

    MS-02 Modify API for service register and bd connection

--- src/MusicStore.Api/Program.cs
+++ src/MusicStore.Api/Program.cs
@@ -1,3 +1,5 @@
+using MusicStore.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register the GenreRepository as a singleton service
+builder.Services.AddSingleton<GenreRepository>(); // Se va a mantener una misma instancia durante toda la vida util de la aplicacion
+
+// AddScoped - Se crea una nueva instancia por cada peticion HTTP
+// AddTransient - Se crea una nueva instancia cada vez que se inyecta el servicio
 
 var app = builder.Build();
 
