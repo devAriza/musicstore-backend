64b773bcf8ae90cc2090333ead57f75a5fc88a2e
JAriza <v.ariza.0511@gmail.com>
2025-09-19 22:57:27 -0600

    MS-03: Add entity and execute migration

---
+++ src/MusicStore.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MusicStore.Entities;
+
+namespace MusicStore.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        // Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // Aplicar automáticamente todas las configuraciones
+
+            //// Custom configurations can be added here
+            //modelBuilder.Entity<Genre>().Property(x=>x.Name).HasMaxLength(50);
+        }
+
+        // Entities to table
+        // public DbSet<Genre> Genres { get; set; }
+
+    }
+}
---
+++ src/MusicStore.Repositories/IGenreRepository.cs
@@ -0,0 +1,15 @@
+﻿using MusicStore.Dto.Request;
+using MusicStore.Dto.Response;
+using MusicStore.Entities;
+
+namespace MusicStore.Repositories
+{
+    public interface IGenreRepository
+    {
+        Task<List<GenreResponseDto>> GetAsync();
+        Task<GenreResponseDto?> GetAsync(int id);
+        Task<int> AddAsync(GenreRequestDto genre);
+        Task UpdateAsync(int id, GenreRequestDto genre);
+        Task DeleteAsync(int id);
+    }
+}
